c5fbe05b18b6fa92c9181d6bdde3484752d8d474
Kirill Tobola <kirilltobola@gmail.com>
2024-06-26 15:31:50 +0800

    update models download urls

--- download.sh
+++ download.sh
@@ -6,10 +6,10 @@ declare download_link;
 if [ "$1" == "table_wise" ];
 then
   file_name="rutabert_table_wise.tar.gz";
-  download_link="https://drive.usercontent.google.com/download?id=1xLaXB9ZJA9G5JpiDP15D8NynzDX1ifWG&export=download&authuser=0&confirm=t&uuid=a35f908b-1469-463f-a6a0-5b9e538ee1c1&at=APZUnTXz_pe1JrgFTieEVijXeEQE:1712834480569";
+  download_link="https://drive.usercontent.google.com/download?id=1HVKSVu_3kpHsu9HryTfAP-uFGlkXuCSQ&export=download&authuser=0&confirm=t&uuid=059405ea-89a4-4d71-bbf5-32bfc85131c5&at=APZUnTUgSTSwYijRC-6AC4j1ixHh%3A1719385959038";
 elif [ "$1" == "column_wise" ]; then
   file_name="rutabert_column_wise.tar.gz";
-  download_link="https://drive.usercontent.google.com/download?id=1yau0IrsRZZdeM0XVfRaN7p6SELgpIpAa&export=download&authuser=0&confirm=t&uuid=d5c6f6f8-da5b-4bc2-bb25-240fa34abd60&at=APZUnTVef6-2yBU3Ukk0oq-e-iUn:1712834703890";
+  download_link="https://drive.usercontent.google.com/download?id=1QXXnmnAhmsmBbn-XxNZHm4WcWMQVJ1w7&export=download&authuser=0&confirm=t&uuid=c196ca4d-b679-4c0a-8374-d00c9926202e&at=APZUnTVnODqRMAwrZsvnOZZeD4-_:1719386096296";
 else echo "Undefined download parameter."; exit;
 fi
 
@@ -18,4 +18,4 @@ echo Downloading $file_name...;
 # Download models
 curl "$download_link" > $file_name;
 mv $file_name ./checkpoints;
-cd checkpoints && tar -xvf $file_name;
+#cd checkpoints && tar -xvf $file_name;
